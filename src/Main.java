6a8349e98554b62fa5f6bfea5c67a3bb146d6945
Evgeniy <evgen03010@gmail.com>
2026-05-01 17:15:23 +0300

    123

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,6 @@ void main() {
         // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
         IO.println("i = " + i + "Привет");
         IO.println("i = " + i + "Привет");
+        IO.println("i = " + i + "Привет");
     }
 }
